622be9f1c53746157ce31677cd18e37547b49cd6
Christopher Dutcher <cadutcher@icloud.com>
2020-08-09 20:43:34 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -20,7 +20,7 @@ end
   module ClassMethods
 
     def metadata
-      "This class produces objects that loves to dance."
+      "This class produces objects that love to dance."
     end
   end
 end
